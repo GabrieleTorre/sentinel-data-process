5590b2681837bfccc42d1675b44c855e4925e418
Gabriele Torre <gabriele.torre@kellify.com>
2022-09-13 18:03:41 +0200

    updated paths

--- scripts/crop_sentinel2_iterate.sh
+++ scripts/crop_sentinel2_iterate.sh
@@ -2,10 +2,10 @@
 
 # out_dir='./tiff-files/'
 
-out_dir='/Volumes/Volume/test-italia-numpy/'
+out_dir='/Volumes/SSD1/Sentinel-2/test-italia-numpy/'
 
 # for f in /Volumes/SSD1/Sentinel-2/2022/*.SAFE;do
-for f in /Volumes/Volume/test-italia/*.SAFE;do
+for f in /Volumes/SSD1/Sentinel-2/test-italia/*.SAFE;do
     echo $f;
     python crop_sentinel2.py $f --directory_out $out_dir;
 done
