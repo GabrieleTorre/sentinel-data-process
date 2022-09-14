c98527d52e0f13d41d1bf795429f7a36dc12ec44
Gabriele Torre <gabriele.torre@kellify.com>
2022-09-14 11:06:38 +0200

    add date interval selection options

--- scripts/crop_sentinel2_iterate.sh
+++ scripts/crop_sentinel2_iterate.sh
@@ -2,10 +2,11 @@
 
 # out_dir='./tiff-files/'
 
-out_dir='/Volumes/SSD1/Sentinel-2/test-italia-numpy/'
+out_dir='/Volumes/SSD1/Sentinel-2/test-italia-numpy/';
+start_date='2021-08-01';
+end_date='2021-11-01';
 
 # for f in /Volumes/SSD1/Sentinel-2/2022/*.SAFE;do
 for f in /Volumes/SSD1/Sentinel-2/test-italia/*.SAFE;do
-    echo $f;
-    python crop_sentinel2.py $f --directory_out $out_dir;
+      python crop_sentinel2.py $f --directory_out $out_dir --start_date $start_date --end_date $end_date;
 done
